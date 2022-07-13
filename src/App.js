87b01d0a42fc875f9d01bfec1824d1676ca32b17
thibaud-iotia <thibaud.charbonnell@gmail.com>
2022-07-13 20:14:21 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./Styles/index.scss"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import MusicDownload from"./Pages/musicDownload"
 import NotFound from "./Pages/NotFound"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' exact="true" element={<MusicDownload/>} />
           <Route path='/Music' exact="true" element={<MusicDownload/>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }

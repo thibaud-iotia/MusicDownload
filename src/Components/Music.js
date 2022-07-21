2eee5a7eeae0d15aa02361298c149f550cb34704
thibaud-iotia <thibaud.charbonnell@gmail.com>
2022-07-21 19:33:04 +0200

    last update

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -5,43 +5,60 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Box } from '@mui/material';
 // import axios from 'axios';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 export default function Music({ videoData }) {
+    //to select file type
+    const [type, setType] = React.useState('');
+
+    const handleChange = (event) => {
+        setType(event.target.value);
+    };
+  //for dialog box
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   // console.log(videoData);
   const data = videoData.snippet;
   const [videoUrl, setVideoUrl] = useState("");
-  // const downloadexe = async (url, path) => {
-  //   try {
-  //     const response = await axios({
-  //       method: "GET",
-  //       url: url
-  //     })
-  //     // Do something with the response
-  //     console.log(response)
-  //   } catch (err) {
-  //     // Handling errors
-  //   }
-  // };
-  const downloadData = async (url) =>{
+  const confirmDownload = () =>{
+    handleClose();
+    console.log(type)
+    //download the video
+    downloadData("http://172.17.224.1:4000/download?URL=" + videoUrl + "&type=" + type + "&title=" + data.title);
+  }
+  const downloadData = async (url) => {
     console.log(url)
     window.location.href = url;
   }
   const downloadVideo = (videoId) => {
     console.log("Video id : " + videoId);
-    // setVideoUrl("https://www.youtube.com/embed/" + videoId);
     setVideoUrl("https://www.youtube.com/watch?v=" + videoId);
-    //download the video
-    downloadData("http://172.17.224.1:4000/download?URL=" + videoUrl,"test");
+    handleClickOpen();
   }
   return (
-    <Box display="inline-block" sx={{width: 600 }}>
-      <Card sx={{ margin : 1 }} onClick={(e) => downloadVideo(videoData.id.videoId)}>
+    <Box display="inline-block" sx={{ width: 600 }}>
+      <Card sx={{ margin: 1, height: 300 }} onClick={(e) => downloadVideo(videoData.id.videoId)}>
         <CardActionArea>
           <CardMedia
             component="img"
             height="140"
-              image={data.thumbnails.high.url}
+            image={data.thumbnails.high.url}
             alt="image of Youtube video"
           />
           <CardContent>
@@ -54,6 +71,37 @@ export default function Music({ videoData }) {
           </CardContent>
         </CardActionArea>
       </Card>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Choose the file type"}
+        </DialogTitle>
+        <DialogContent sx={{ marginTop: 1 }}>
+          <FormControl sx={{ m: 1, minWidth: 150 }}>
+            <InputLabel id="fileType" >File</InputLabel>
+            <Select
+              labelId="fileType"
+              id="selectType"
+              value={type}
+              label="Age"
+              onChange={handleChange}
+            >
+              <MenuItem value={"mp4"}>Mp4</MenuItem>
+              <MenuItem value={"mp3"}>Mp3</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={confirmDownload} autoFocus>
+            Download
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
 
   );
--- src/Components/Musics.js
+++ src/Components/Musics.js
@@ -3,16 +3,18 @@ import React, { useState } from 'react';
 import SearchBar from "material-ui-search-bar";
 import Music from './Music';
 
+
 const Musics = () => {
+    //to set research
     const [searched, setSearched] = useState("");
     const [result, setResult] = useState([]);
     //send request
-    const setRequestSearch = (newValue) =>{
+    const setRequestSearch = (newValue) => {
         setSearched(newValue);
-        if (result !== ""){
+        if (result !== "") {
             axios
-            .get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" + searched +"&key=" + process.env.REACT_APP_YOUTUBE_API_KEY)
-            .then((res) => setResult(res.data.items));  
+                .get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + searched + "&key=" + process.env.REACT_APP_YOUTUBE_API_KEY)
+                .then((res) => setResult(res.data.items));
         }
     }
     //reset research
@@ -28,11 +30,11 @@ const Musics = () => {
                 placeholder="Press enter to search"
             />
             {result.map((video, id) => (
-                <Music key={id} videoData={video}/>
+                <Music key={id} videoData={video} />
             ))}
         </div>
     );
-    
+
 };
 
 export default Musics;
